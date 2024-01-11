3349409b0722ebcb9ca39a0a40f2553df052d129
Evgeniy Korobitsin <evgen-kb@mail.ru>
2024-01-11 16:44:22 +0500

    Второй коммит

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+  var audio = $('#audioTrack')[0];
 
+  $playBtn = $('#audio-play');
+  $pauseayBtn = $('#audio-pause');
+  $audioControl = $('[data-type="audio-control"');
+
+  // audio.play();
+
+  setTimeout(initAudioPlayer, 800);
+
+  function initAudioPlayer() {
+    $pauseayBtn.parent().show();
+    audio.loop = true;
+    audio.play();
+    audio.volume = .1;
+
+    $audioControl.click(function () {
+      if (audio.paused) {
+        audio.play();
+        $playBtn.parent().hide();
+        $pauseayBtn.parent().show();
+      } else {
+        audio.pause();
+        $playBtn.parent().show();
+        $pauseayBtn.parent().hide();
+      }
+    });
+  }
 
 });
